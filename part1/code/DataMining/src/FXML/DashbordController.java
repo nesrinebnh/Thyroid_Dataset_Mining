e72b43c93981b5ad13a5a794e4723cfc1d7d5e99
nesrine <43906541+nesrinebnh@users.noreply.github.com>
2021-01-03 07:45:43 +0100

    Dashbord ui+code

---
+++ part1/code/DataMining/src/FXML/DashbordController.java
@@ -0,0 +1,134 @@
+package FXML;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.ResourceBundle;
+import java.util.Vector;
+
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.chart.PieChart;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
+public class DashbordController implements Initializable {
+
+   @FXML
+   private TextField title;
+   @FXML
+   private PieChart chart;
+
+   @FXML
+   private TableView<Attribute> table;
+
+   @FXML
+   private TableColumn<Attribute, String> attr1;
+
+   @FXML
+   private TableColumn<Attribute, String> attr2;
+
+   @FXML
+   private TableColumn<Attribute, String> attr3;
+
+   @FXML
+   private TableColumn<Attribute, String> attr4;
+
+   @FXML
+   private TableColumn<Attribute, String> attr5;
+
+   @FXML
+   private TableColumn<Attribute, String> target;
+
+   @FXML
+   private TableView<Attribute> table2;
+
+   @FXML
+   private TableColumn<Attribute, String> name;
+
+   @FXML
+   private TableColumn<Attribute, String> min;
+
+   @FXML
+   private TableColumn<Attribute, String> max;
+
+   @FXML
+   private TableColumn<Attribute, String> mode;
+
+   @FXML
+   private TableColumn<Attribute, String> mediane;
+
+   private ObservableList<Attribute> attributeData;
+   private ObservableList<Attribute> statData;
+
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		// TODO Auto-generated method stub
+		attributeData = FXCollections.observableArrayList();
+		statData = FXCollections.observableArrayList();
+		title.setEditable(false);
+		ObservableList<PieChart.Data> pieChartData =
+                FXCollections.observableArrayList(
+                		new PieChart.Data("1. normal", 150),
+                		new PieChart.Data("2. hyper", 35),
+                		new PieChart.Data("3. hypo", 30));
+		this.chart.getData().addAll(pieChartData);
+
+		BufferedReader txtReader;
+		try {
+			txtReader = new BufferedReader(new FileReader("src/FXML/Thyroid_Dataset.txt"));
+			String row;
+			try {
+				while ((row = txtReader.readLine()) != null) {
+				    String[] data = row.split(",");
+				    String target = data[0];
+				    String attr1 = data[1];
+				    String attr2 = data[2];
+				    String attr3 = data[3];
+				    String attr4 = data[4];
+				    String attr5 = data[5];
+				    attributeData.add(new Attribute(attr1, attr2, attr3, attr4, attr5, target));
+				}
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			txtReader.close();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		attr1.setCellValueFactory(cellData -> cellData.getValue().attr1Property());
+		attr2.setCellValueFactory(cellData -> cellData.getValue().attr2Property());
+		attr3.setCellValueFactory(cellData -> cellData.getValue().attr3Property());
+		attr4.setCellValueFactory(cellData -> cellData.getValue().attr4Property());
+		attr5.setCellValueFactory(cellData -> cellData.getValue().attr5Property());
+		target.setCellValueFactory(cellData -> cellData.getValue().targetProperty());
+		System.out.println(this.attributeData.size());
+		table.setItems(attributeData);
+
+		for(int i=1; i<=5;i++){
+			statData.add(new Attribute(String.valueOf(i), "25.5", "35", "117", "15"));
+		}
+
+		name.setCellValueFactory(cellData -> cellData.getValue().nameProperty());
+		min.setCellValueFactory(cellData -> cellData.getValue().minProperty());
+		max.setCellValueFactory(cellData -> cellData.getValue().maxProperty());
+		mode.setCellValueFactory(cellData -> cellData.getValue().modeProperty());
+		mediane.setCellValueFactory(cellData -> cellData.getValue().medianeProperty());
+		table2.setItems(statData);
+
+	}
+
+}
---
+++ part1/code/DataMining/src/application/Main.java
@@ -0,0 +1,35 @@
+package application;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+
+
+public class Main extends Application {
+
+
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+            // Load root layout from fxml file.
+			Parent root = FXMLLoader.load(getClass().getResource("/FXML/Dashbord.fxml"));
+            Scene scene = new Scene(root);
+			//scene.getStylesheets().add(getClass().getResource("/Font/Dashbord.css").toExternalForm());
+
+            scene.getStylesheets().add("/Font/Dashbord.css");
+            primaryStage.setScene(scene);
+            primaryStage.show();
+            primaryStage.setResizable(false);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
